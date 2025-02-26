abf11fa3fa0b4dec2b96d236b1ad819d9baa82dc
DiilaNa <liyanaarachchidilan@gmail.com>
2025-02-26 14:55:13 +0530

    Congig added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,12 @@ module lk.ijse.gdse.supermarket {
     requires java.mail;
     requires mysql.connector.j;
     requires org.apache.commons.logging;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires java.naming;
 
     opens lk.ijse.gdse.supermarket.dto.tm to javafx.base;
     opens lk.ijse.gdse.supermarket.controller to javafx.fxml;
+    opens lk.ijse.gdse.supermarket.entity to org.hibernate.orm.core; //entity tik coonnect kala
     exports lk.ijse.gdse.supermarket;
 }

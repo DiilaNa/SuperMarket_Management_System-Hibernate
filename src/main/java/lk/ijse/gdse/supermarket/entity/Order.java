e440d3c64c497b203548882946a3e8e02f0fdc82
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-04 18:49:23 +0530

    Order Entity Updated

--- src/main/java/lk/ijse/gdse/supermarket/entity/Order.java
+++ src/main/java/lk/ijse/gdse/supermarket/entity/Order.java
@@ -1,24 +1,30 @@
 package lk.ijse.gdse.supermarket.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
+
 
 import java.sql.Date;
 
 
 // Lombok annotations to generate methods automatically and reduce boilerplate code
-@Getter                 // @Getter: Automatically generates getter methods for all fields
-@Setter                 // @Setter: Automatically generates setter methods for all fields
 @AllArgsConstructor     // @AllArgsConstructor: Generates a constructor with all fields as parameters
 @NoArgsConstructor      // @NoArgsConstructor: Generates a no-argument constructor
-@ToString               // @ToString: Automatically generates a string representation of the object
-
-
+@Data
+@Entity
+@Table(name = "orders")
 public class Order {
-
+    @Id
+    @Column(name = "order_id")
     private String orderId;
     private String customerId;
     private Date orderDate;
+    @ManyToOne
+    @JoinColumn(name = "cust_id")
+    private Customer customer;
+
+
 
     // @orderDetailsDTOS: A list of OrderDetailsDTO objects, each representing an item in the order
 }
